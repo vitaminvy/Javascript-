d1f87d9e1604ce5a051a2a561ab03c0b77a317ca
titvvn <hovuongtuongvy2711@gmail.com>
2025-07-01 18:16:21 +0700

    Javascript built-in data structure - Number

---
+++ Javascript built-in data structure - Number/number.js
@@ -0,0 +1,8 @@
+let num1 = 1;
+let strNum1 = num1.toString();
+console.log ('strNum1: ', strNum1) //output : strNum1:  1
+console.log ('num1: ', num1); //output: num1:  1
+console.log ('typeof strNum1: ', typeof strNum1) //typeof strNum1: string
+console.log ('typeof num1: ', typeof num1) //typeof strNum1: number
+
+
