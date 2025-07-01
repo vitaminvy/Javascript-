abe2d11f8569ed47a85b7f291d64473f30ee1f11
titvvn <hovuongtuongvy2711@gmail.com>
2025-07-01 20:14:07 +0700

    add substring, slice method

--- Javascript_built_in_data_structure_String/stringSplit.js
+++ Javascript_built_in_data_structure_String/stringSplit.js
@@ -2,3 +2,12 @@ let myStr = "hello world, from SESV!";
 console.log (myStr.split(',')); // [ 'hello world', ' from SESV!' ]
 console.log (myStr.split(' ')); // [ 'hello', 'world,', 'from', 'SESV!']
 console.log (myStr); //hello world, from SESV!
+
+//substring 
+console.log (myStr.substring(-1,-5)); // trả về chuỗi rỗng
+console.log (myStr.substring(-1,5)) ; // hello 
+console.log (myStr.substring(5,1)) ; // ello
+//Slice
+console.log (myStr.slice(-1,5)); // trả về chuỗi rỗng
+console.log (myStr.slice(-23,5)); // hello
+
