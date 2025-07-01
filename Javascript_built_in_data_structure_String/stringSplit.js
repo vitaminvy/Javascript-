7b400e9d84ea8c32c7acd7a81a93fb496373199b
titvvn <hovuongtuongvy2711@gmail.com>
2025-07-01 19:54:56 +0700

    add stringSplit.js

---
+++ Javascript_built_in_data_structure_String/stringSplit.js
@@ -0,0 +1,4 @@
+let myStr = "hello world, from SESV!";
+console.log (myStr.split(',')); // [ 'hello world', ' from SESV!' ]
+console.log (myStr.split(' ')); // [ 'hello', 'world,', 'from', 'SESV!']
+console.log (myStr); //hello world, from SESV!
